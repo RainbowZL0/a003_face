2b5142ff6ae850ae8be50f7917f03f7047ee77f7
RainbowZL <mkkx46733691@gmail.com>
2024-12-13 19:25:19 +0800

    将测试mtcnn的图片结果文件夹纳入git管理，只管理其内的.gitkeep文件，图片本身不受git管理

--- a001_test/a003_facenet/a002_mtcnn_only.py
+++ a001_test/a003_facenet/a002_mtcnn_only.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import cv2
 import numpy as np
 from facenet_pytorch.models.mtcnn import MTCNN
@@ -7,15 +10,17 @@ from tqdm import tqdm
 from a002_main.a001_utils.a002_general_utils import glob_png_paths_in_folder
 
 IMAGE_FOLDER = "a002_main/a004_fastapi/a001_images/a003_upload_images_no_repeat"
-CROP_IMAGE_FOLDER = r"a001_test/a003_facenet/a001_mtcnn_only"
-mtcnn = MTCNN()
+SUCCESS_CROP_IMAGE_FOLDER = r"a001_test/a003_facenet/a001_mtcnn_success_crop"
+FAIL_CROP_IMAGE_FOLDER = r"a001_test/a003_facenet/a002_fail_crop"
+mtcnn = MTCNN(thresholds=[0.4, 0.5, 0.5])
 
 def start():
     image_paths_list = glob_png_paths_in_folder(
         image_folder=IMAGE_FOLDER
     )
 
-    fail_count = 0
+    fail_crop_image_paths_list = []
+    success_crop_image_paths_list = []
 
     for image_path in tqdm(image_paths_list):
         image_array = cv2.imread(image_path)  # hwc, bgr, uint8
@@ -25,21 +30,32 @@ def start():
         image_array = np.tile(image_array, (1, 1, 3))
 
         rst_tensor = mtcnn(image_array)  # chw
+
         if rst_tensor is not None:
             rst_array = rst_tensor.numpy()
             rst_array = rst_array.transpose((1, 2, 0))  # hwc
             rst_array = (rst_array * 128 + 127.5).astype(np.uint8)
 
-            plt.imshow(rst_array)
-            plt.axis("off")
-            plt.show()
-            plt.close()
+            success_crop_image_paths_list.append(image_path)
+            plt.imsave(
+                fname=os.path.join(SUCCESS_CROP_IMAGE_FOLDER, os.path.basename(image_path)),
+                arr=rst_array,
+            )
+            # plt.imshow(rst_array)
+            # plt.axis("off")
+            # plt.show()
+            # plt.close()
 
         else:
-            fail_count += 1
+            fail_crop_image_paths_list.append(image_path)
+            shutil.copyfile(
+                src=image_path,
+                dst=os.path.join(FAIL_CROP_IMAGE_FOLDER, os.path.basename(image_path)),
+            )
+
 
     print(
-        f"fails: {fail_count} / {len(image_paths_list)}"
+        f"fails: {len(fail_crop_image_paths_list)} / {len(image_paths_list)}"
     )
 
 
--- a001_test/a006_retina_face_test/a001.py
+++ a001_test/a006_retina_face_test/a001.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from a002_main.a001_utils.a002_general_utils import glob_png_paths_in_folder
 
 IMAGE_FOLDER = "a002_main/a004_fastapi/a001_images/a003_upload_images_no_repeat"
-CROP_IMAGE_FOLDER = r"a001_test/a003_facenet/a001_mtcnn_only"
+CROP_IMAGE_FOLDER = r"a001_test/a003_facenet/a001_mtcnn_success_crop"
 
 model = get_model("resnet50_2020-07-20", max_size=2048)
 model.eval()
